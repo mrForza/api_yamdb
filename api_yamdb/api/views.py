e7943ddb2f2b5fc91e16c50a483338d886e52e6b
Roman Gromov <gromov1921@bk.ru>
2023-07-29 17:41:38 +0300

    undo merging

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -12,7 +12,8 @@ from django.core.mail import send_mail
 
 from reviews.models import User
 from api.serializers import (
-    UserSerializer, SignSerializer, MeUserSerializer, TokenSerializer
+    UserSerializer, SignSerializer,
+    MeUserSerializer, TokenSerializer
 )
 from api.permissions import IsAdmin
 
